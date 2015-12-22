9e661c3e80e05c70e998421b9737b5e126f834f5
tianxiangbing <55342775@qq.com>
2015-12-22 10:58:06 +0800

    添加cmd标准的，修改未选择时出现的bug

--- src/mobile-select-area.js
+++ src/mobile-select-area.js
@@ -11,7 +11,13 @@
 	//amd
 	if (typeof define === 'function' && define.amd) {
 		define(['$', 'dialog'], factory);
-	} else if (typeof exports === 'object') { //umd
+	}else if (typeof define === 'function' && define.cmd) {
+		define(function(require, exports, module) {
+			var $ = require("$");
+			var Dialog=require("dialog.js");
+			return factory($,Dialog);
+		});
+	}  else if (typeof exports === 'object') { //umd
 		module.exports = factory();
 	} else {
 		root.MobileSelectArea = factory(window.Zepto || window.jQuery || $);
@@ -25,6 +31,7 @@
 		this.index = 0;
 		this.value = [0, 0, 0];
 		this.oldvalue;
+		this.oldtext;
 		this.text = ['', '', ''];
 		this.level = 3;
 		this.mtop = 30;
@@ -32,9 +39,11 @@
 	};
 	MobileSelectArea.prototype = {
 		init: function(settings) {
-			this.settings = $.extend({eventName:'click'}, settings);
+			this.settings = $.extend({
+				eventName: 'click'
+			}, settings);
 			this.trigger = $(this.settings.trigger);
-			this.settings.default==undefined ? this.default=1:this.default = 0 ;//0为空,1时默认选中第一项
+			this.settings.default == undefined ? this.default = 1 : this.default = 0; //0为空,1时默认选中第一项
 			level = parseInt(this.settings.level);
 			this.level = level > 0 ? level : 3;
 			this.trigger.attr("readonly", "readonly");
@@ -43,13 +52,15 @@
 			this.oldvalue = this.value.concat([]);
 			this.clientHeight = document.documentElement.clientHeight || document.body.clientHeight;
 			this.clientWidth = document.documentElement.clientWidth || document.body.clientWidth;
-			this.getData();
+			// this.promise = this.getData();
 			this.bindEvent();
 		},
 		getData: function() {
 			var _this = this;
+			var dtd = $.Deferred();
 			if (typeof this.settings.data == "object") {
 				this.data = this.settings.data;
+				dtd.resolve();
 			} else {
 				$.ajax({
 					dataType: 'json',
@@ -58,12 +69,14 @@
 					type: 'GET',
 					success: function(result) {
 						_this.data = result.data;
+						dtd.resolve();
 					},
 					accepts: {
 						json: "application/json, text/javascript, */*; q=0.01"
 					}
 				});
 			}
+			return dtd;
 		},
 		bindEvent: function() {
 			var _this = this;
@@ -81,18 +94,20 @@
 					}
 					this.dispose();
 				}, {
-					width:320,
-					height:215
+					width: 320,
+					height: 215
 				});
 				_this.scroller = $('#' + _this.id);
-				_this.format();
+				_this.getData().done(function() {
+					_this.format();
+				});
 				var start = 0,
 					end = 0
 				_this.scroller.children().bind('touchstart', function(e) {
-					start = (e.changedTouches||e.originalEvent.changedTouches)[0].pageY;
+					start = (e.changedTouches || e.originalEvent.changedTouches)[0].pageY;
 				});
 				_this.scroller.children().bind('touchmove', function(e) {
-					end = (e.changedTouches||e.originalEvent.changedTouches)[0].pageY;
+					end = (e.changedTouches || e.originalEvent.changedTouches)[0].pageY;
 					var diff = end - start;
 					var dl = $(e.target).parent();
 					if (dl[0].nodeName != "DL") {
@@ -104,7 +119,7 @@
 					return false;
 				});
 				_this.scroller.children().bind('touchend', function(e) {
-					end = (e.changedTouches||e.originalEvent.changedTouches)[0].pageY;
+					end = (e.changedTouches || e.originalEvent.changedTouches)[0].pageY;
 					var diff = end - start;
 					var dl = $(e.target).parent();
 					if (dl[0].nodeName != "DL") {
@@ -144,6 +159,7 @@
 			var _this = this;
 			var child = _this.scroller.children();
 			this.f(this.data);
+			console.log(_this.text)
 		},
 		f: function(data) {
 			var _this = this;
@@ -154,7 +170,7 @@
 			var str = '<dl><dd ref="0">——</dd>';
 			var focus = 0,
 				childData, top = _this.mtop;
-			if (_this.index !== 0 && _this.value[_this.index - 1] == "0" && this.default==0) {
+			if (_this.index !== 0 && _this.value[_this.index - 1] == "0" && this.default == 0) {
 				str = '<dl><dd ref="0" class="focus">——</dd>';
 				_this.value[_this.index] = 0;
 				_this.text[_this.index] = "";
@@ -164,14 +180,14 @@
 					str = '<dl><dd ref="0" class="focus">——</dd>';
 					focus = 0;
 				}
-				if(item.length>0 && this.default==1){
+				if (item.length > 0 && this.default == 1) {
 					str = '<dl>';
 					var pid = item[0].pid || 0;
 					var id = item[0].id || 0;
 					focus = item[0].id;
 					childData = item[0].child;
-					if(!_this.value[this.index ]){
-						_this.value[this.index ] = id;
+					if (!_this.value[this.index]) {
+						_this.value[this.index] = id;
 						_this.text[this.index] = item[0].name;
 					}
 					str += '<dd pid="' + pid + '" class="' + cls + '" ref="' + id + '">' + item[0].name + '</dd>';
@@ -184,7 +200,7 @@
 						cls = "focus";
 						focus = id;
 						childData = item[j].child;
-						top = _this.mtop * (-(j-_this.default));
+						top = _this.mtop * (-(j - _this.default));
 					};
 					str += '<dd pid="' + pid + '" class="' + cls + '" ref="' + id + '">' + item[j].name + '</dd>';
 				}
@@ -203,16 +219,18 @@
 		},
 		submit: function() {
 			this.oldvalue = this.value.concat([]);
+			this.oldtext = this.text.concat([]);
 			if (this.trigger[0].nodeType == 1) {
 				//input
 				this.trigger.val(this.text.join(this.separator));
 				this.trigger.attr('data-value', this.value.join(','));
 			}
 			this.trigger.next(':hidden').val(this.value.join(','));
-			this.settings.callback && this.settings.callback.call(this,this.scroller,this.text,this.value);
+			this.settings.callback && this.settings.callback.call(this, this.scroller, this.text, this.value);
 		},
 		cancel: function() {
 			this.value = this.oldvalue.concat([]);
+			this.text = this.oldtext.concat([]);
 		}
 	};
 	return MobileSelectArea;
